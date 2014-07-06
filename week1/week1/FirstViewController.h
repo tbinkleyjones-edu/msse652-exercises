b9e0d5485162fa4fd30c1fdaf2e33c88fe57d902
Tim <tbinkleyjones@regis.edu>
2014-07-06 15:15:15 -0600

    week 1 exercise

---
+++ week1/week1/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  week1
+//
+//  Created by Tim Binkley-Jones on 7/5/14.
+//  Copyright (c) 2014 msse652. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+- (IBAction) exitHere:(UIStoryboardSegue *)sender;
+
+@end
---
+++ week1/week1/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  week1
+//
+//  Created by Tim Binkley-Jones on 7/5/14.
+//  Copyright (c) 2014 msse652. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
